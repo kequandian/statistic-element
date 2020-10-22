4f5a47667877a36320f9e96280478e87eaddca0c
LLH <youyeke@163.com>
2020-10-22 17:53:01 +0800

    将 promiseAjax 整合进 APIContainer

--- src/APIContainer.js
+++ src/APIContainer.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 export default function APIContainer(props) {
   const [data, setData] = useState({});
-  const { API, queryData, query, children } = props;
+  const { API, queryData, children } = props;
 
   useEffect(_ => {
-    query(API, queryData)
+    promiseAjax(API, queryData)
       .then(responseData => {
         console.log('request rst: ', responseData);
 
@@ -17,5 +17,40 @@ export default function APIContainer(props) {
 
   return React.cloneElement(children, {
     data: data,
+  })
+}
+
+function promiseAjax(url, data, options = {}) {
+  const { method = 'GET', async = true } = options;
+
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, async);
+    xhr.responseType = 'JSON';
+
+    xhr.onreadystatechange = () => {
+
+      if (xhr.readyState !== 4) {
+        return;
+      }
+
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        let result
+        try {
+          result = JSON.parse(xhr.responseText);
+          resolve(result);
+
+        } catch (error) {
+          reject("返回的数据非 json 格式");
+        }
+      } else {
+        reject(xhr.statusText);
+      }
+    }
+    xhr.onerror = (err) => {
+      reject(err);
+    }
+
+    xhr.send(data);
   })
 }
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Statistic from '@/Statistic';
 ReactDOM.render(
   <APIContainer
     API={'http://test.com/api/test'}
-    query={PromiseAjax}
     queryData={{
       extraData: 'test',
     }}
@@ -16,40 +15,3 @@ ReactDOM.render(
   </APIContainer>
   , document.getElementById("root")
 );
-
-
-
-function PromiseAjax(url, data, options = {}) {
-  const { method = 'GET', async = true } = options;
-
-  return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open(method, url, async);
-    xhr.responseType = 'JSON';
-
-    xhr.onreadystatechange = () => {
-
-      if (xhr.readyState !== 4) {
-        return;
-      }
-
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        let result
-        try {
-          result = JSON.parse(xhr.responseText);
-          resolve(result);
-
-        } catch (error) {
-          reject("返回的数据非 json 格式");
-        }
-      } else {
-        reject(xhr.statusText);
-      }
-    }
-    xhr.onerror = (err) => {
-      reject(err);
-    }
-
-    xhr.send(data);
-  })
-}
