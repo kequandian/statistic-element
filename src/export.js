069fc19b3c6957a668807fe1a7c5eebbd02de1df
HDJ <263463764@qq.com>
2022-03-23 14:59:24 +0800

    更改 statistic 首字母大写=>小写

--- src/export.js
+++ src/export.js
@@ -7,7 +7,7 @@ import Pie from '@/components/general-chart/Pie';
 import LineGrid from '@/components/lineGrid/LineGrid';
 import NumberRows from '@/components/numberRows/NumberRows';
 import ColumnChain from '@/components/columnChain/ColumnChain';
-import Statistic from '@/Statistic';
+import Statistic from '@/statistic';
 import Radar from '@/components/radar/Radar';
 import Instrument from '@/components/instrument/Instrument';
 // import TableStatistic from '@/components/tableStatistic/TableStatistic';
